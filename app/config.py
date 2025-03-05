a9b65edb4445b68475587bebddf5e48542484cd6
Umesh Konduru <51374009+UmeshKonduru@users.noreply.github.com>
2025-03-05 18:36:35 +0530

    Updated requirements

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str = "secret"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    UPLOAD_DIR: str = "../uploads"
+    UPLOAD_DIR: str = "./uploads"
     
     class Config:
         env_file = ".env"
