5ba0f84add927fd215efc0738be968ee262cff91
Umesh Konduru <51374009+UmeshKonduru@users.noreply.github.com>
2025-05-12 01:08:59 +0530

    trying again

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://umeshkonduru.github.io/iot_testbed_dep/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
